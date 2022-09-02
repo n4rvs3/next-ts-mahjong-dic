1bede562b32b710816709a3ee61691c756c87757
Ibuki-Matsumoto <n4rvs3apex@gmail.com>
2022-09-02 12:00:20 +0900

    fix for vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,17 @@ import type { NextPage, InferGetStaticPropsType } from 'next'
 
 import { mahjong_pai } from "@prisma/client";
 
-type Props = InferGetStaticPropsType<typeof getStaticProps>
+// type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-const Home: NextPage<Props> = ({ pai }) => {
+// const Home: NextPage<Props> = ({ pai }) => {
+const Home = () => {
 
-  console.log(pai)
+  // console.log(pai)
 
   return (
     <div >
       <p>hello world</p>
-      <div>
+      {/* <div>
         {pai.map((res => (
           <div
             key={res.id}
@@ -21,20 +22,20 @@ const Home: NextPage<Props> = ({ pai }) => {
             <p className='text-lg my-2'>{res.name}</p>
           </div>
         )))}
-      </div>
+      </div> */}
     </div>
   )
 }
 
 export default Home
 
-export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:3000/api/mahjong')
-  const pai: mahjong_pai[] = await res.json()
+// export const getStaticProps = async () => {
+//   const res = await fetch('http://localhost:3000/api/mahjong')
+//   const pai: mahjong_pai[] = await res.json()
 
-  return {
-    props: {
-      pai,
-    },
-  }
-}
+//   return {
+//     props: {
+//       pai,
+//     },
+//   }
+// }
